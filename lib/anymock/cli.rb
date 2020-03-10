2cdbd31163f8c611732af4a9bdbc4592b2513031
Jun Yokoyama <jun@larus.org>
2020-03-11 01:34:24 +0900

    chnage "-h" option to "-a"

--- lib/anymock/cli.rb
+++ lib/anymock/cli.rb
@@ -5,7 +5,7 @@ module Anymock
     default_command :start_server
 
     option :d, type: :string, default: 'public/'
-    option :h, type: :string, default: 'localhost'
+    option :a, type: :string, default: 'localhost'
     option :p, type: :string, default: '8080'
     option :r, type: :string, default: nil
     option :m, type: :boolean, default: false
@@ -25,7 +25,7 @@ module Anymock
     def option_strings
       {
         document_root: options[:d],
-        address: options[:h],
+        address: options[:a],
         port: options[:p],
         response: options[:r],
         mirror: options[:m]
